af53cf3c6ed09e64d9f5cde64bd6de2668d602bf
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-02-09 22:00:51 +0500

    12

--- TelegramBot/Domain/ApplicationContext.cs
+++ TelegramBot/Domain/ApplicationContext.cs
@@ -20,7 +20,7 @@ namespace TelegramBot.Domain
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlite("Data Source=data/mydatabase.db");
+                optionsBuilder.UseSqlite(Environment.GetEnvironmentVariable("CONNECTIONSTRING"));
             }
 
             base.OnConfiguring(optionsBuilder);
